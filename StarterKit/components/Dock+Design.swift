acf285e3007b2e8e5b6773e89f96efe92d4414ee
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-04-22 16:30:04 +0000

    Update starter kit for v1.73.1-rc.0

---
+++ StarterKit/components/Dock+Design.swift
@@ -0,0 +1,21 @@
+import IMGLYEditor
+
+// MARK: - Dock
+
+extension DesignEditorConfiguration {
+  /// The default dock configuration.
+  static var defaultDock: Dock.Configuration {
+    Dock.Configuration { builder in
+      builder.items { _ in
+        Dock.Buttons.elementsLibrary()
+        Dock.Buttons.photoRoll()
+        Dock.Buttons.systemCamera()
+        Dock.Buttons.imagesLibrary()
+        Dock.Buttons.textLibrary()
+        Dock.Buttons.shapesLibrary()
+        Dock.Buttons.stickersLibrary()
+        Dock.Buttons.resize()
+      }
+    }
+  }
+}
